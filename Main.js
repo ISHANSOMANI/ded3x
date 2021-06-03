a974b497534d32f5e466a019d9da018fdfde45b9
ISHANSOMANI <73775802+ISHANSOMANI@users.noreply.github.com>
2021-06-03 14:56:51 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,19 @@
+var Ishan = "Ishan";
+var Somani = "Somani";
+var Final_Name = Ishan.concat(Somani);
+
+console.log(Final_Name);
+
+var N1 = 30000000;
+var N2  = 6;
+var Add = N1+N2;
+var Minus = N1-N2;
+var X = N1*N2;
+var di = N1/N2;
+
+console.log("Number one is "+N1);
+console.log("Number two is "+N2);
+console.log("Addition"+Add);
+console.log("Subtraction"+Minus);
+console.log("multiplication"+X);
+console.log("division"+di);
